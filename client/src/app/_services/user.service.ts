78fbfbe87503645af51b289f8b53a1448e98aec7
patj_ryan <patj_ryan@yahoo.com>
2021-08-03 15:15:03 -0700

    quick bug fixes

--- client/src/app/_services/user.service.ts
+++ client/src/app/_services/user.service.ts
@@ -1,25 +1,31 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
-const httpOptions = {
-  headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
-  })
-}
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
   baseUrl = 'https://localhost:5001/api/';
-
-
+  httpOptions = {};
   constructor(private http:HttpClient) { }
   getUsers() {
-    return this.http.get(this.baseUrl + "users", httpOptions)
+    this.setHttpOptions();
+    return this.http.get(this.baseUrl + "users", this.httpOptions)
   }
 
   getUser(id) {
-    return this.http.get(this.baseUrl + "users/" + id, httpOptions)
+    this.setHttpOptions
+    return this.http.get(this.baseUrl + "users/" + id, this.httpOptions)
   }
-
+  
+  private setHttpOptions() {
+    let user = JSON.parse(localStorage.getItem('user'));
+    if (user) {
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+        })
+      }
+    }
+  }
 }
